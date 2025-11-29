99ca447b3866b0dd8c10957c0435ab1690da71f3
Garima Bansal <bansalgarima689@gmail.com>
2025-11-30 03:08:10 +0530

    add

---
+++ src/ControlFlow/StepsToReduceToZero.java
@@ -0,0 +1,21 @@
+package ControlFlow;
+import java.util.*;
+
+
+public class StepsToReduceToZero {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int count=0;
+        while(num>0){
+            if(num%2==0) {
+                num=num/2;
+            }
+            else{
+                num=num-1;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
